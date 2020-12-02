9d01e01bd239473123b9983f5e3fd0e62bffae31
Dien <dien25888@gmail.com>
2020-12-02 14:16:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    //nav-bautton phone
+    $('.nav-button').click(function() {
+        $('.nav-button').toggleClass('change');
+    });
+
+    //zoom in and out nav menu 
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if (position >= 1200) {
+            $('.nav-menu').addClass('custom-navbar');
+        } else {
+            $('nav-menu').removeClass('custom-navbar');
+        }
+    });
+});
