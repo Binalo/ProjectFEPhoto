e0448df58e94c61a97d79c9a8e9072d9a52af35b
Dien <dien25888@gmail.com>
2020-12-03 10:57:21 +0700

    third commit

--- script.js
+++ script.js
@@ -13,4 +13,15 @@ $(document).ready(function() {
             $('nav-menu').removeClass('custom-navbar');
         }
     });
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if (position >= 1110 || position <= 0) {
+            $('.camera-img').addClass('moveLeft');
+            $('.mission-text').addClass('moveRight');
+
+        } else {
+            $('.camera-img').removeClass('moveLeft');
+            $('.mission-text').removeClass('moveRight');
+        }
+    });
 });
